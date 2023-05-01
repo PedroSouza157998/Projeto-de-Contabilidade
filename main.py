ebf6bb1fad68e7ba1d391bae250a2c9c677c981e
Geovanna Domingos <gmdomingosn@gmail.com>
2023-05-01 20:31:54 -0300

    adiciona input para o estado e adiciona variável do estado

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from js import console
 # console.log("teste")
 
 
-def visualizar_tabela(*ags, **kags):
+def visualizar_tabela():
     input_municipio = Element("input_municipio")
+    input_estado = Element("input_estado")
     municipio = input_municipio.element.value
-    alert("o municipio inserido foi "+municipio)
+    estado = input_municipio.element.value
+    alert("o municipio inserido foi " + municipio + " - " + estado)
     input_municipio.element.value = ""
+    input_estado.element.value = ""
